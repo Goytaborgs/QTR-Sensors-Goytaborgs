233c76cc5b543ecb12abce10efecf3d40c48b2fd
CaioGazen <61031028+CaioGazen@users.noreply.github.com>
2024-11-08 12:41:59 +0000

    Deploying to gh-pages from @ Goytaborgs/QTR-Sensors-Goytaborgs@879a53fed1cfbce0d64df87c88dcce322f7c2820 🚀

--- search/all_12.js
+++ search/all_12.js
@@ -2,11 +2,12 @@ var searchData=
 [
   ['read_0',['read',['../classQTRSensors.html#a92f25ec98543d6e87bf65a9db67c69ef',1,'QTRSensors::read()'],['../structcmd_8h.html#a9c7b76d5266903891c803132d51ccb90',1,'read():&#160;structcmd.h']]],
   ['readcalibrated_1',['readCalibrated',['../classQTRSensors.html#aa2f5d7779744e5d93f6935d2cdf589eb',1,'QTRSensors']]],
-  ['reading_20the_20sensors_2',['Reading the sensors',['../md_usage.html#autotoc_md10',1,'']]],
-  ['readlineblack_3',['readLineBlack',['../classQTRSensors.html#a8f2a5239ae547928284c5f81cd7ec89c',1,'QTRSensors']]],
-  ['readlinewhite_4',['readLineWhite',['../classQTRSensors.html#a231f1ef20c2a7ba325bdb794fc2dedf9',1,'QTRSensors']]],
-  ['reflectance_20sensors_5',['Arduino library for the Pololu QTR Reflectance Sensors',['../index.html',1,'']]],
-  ['releaseemitterpins_6',['releaseEmitterPins',['../classQTRSensors.html#af4fea64349f7d85830b41ded4aa63ce8',1,'QTRSensors']]],
-  ['resetcalibration_7',['resetCalibration',['../classQTRSensors.html#aa840b6ef17562d41edf21ddd08e0672e',1,'QTRSensors']]],
-  ['restypedef_2ecpp_8',['restypedef.cpp',['../restypedef_8cpp.html',1,'']]]
+  ['readcalibration_2',['readCalibration',['../classQTRSensors.html#a7a256838a32ac8dff6989f06dda040f8',1,'QTRSensors']]],
+  ['reading_20the_20sensors_3',['Reading the sensors',['../md_usage.html#autotoc_md10',1,'']]],
+  ['readlineblack_4',['readLineBlack',['../classQTRSensors.html#a8f2a5239ae547928284c5f81cd7ec89c',1,'QTRSensors']]],
+  ['readlinewhite_5',['readLineWhite',['../classQTRSensors.html#a231f1ef20c2a7ba325bdb794fc2dedf9',1,'QTRSensors']]],
+  ['reflectance_20sensors_6',['Arduino library for the Pololu QTR Reflectance Sensors',['../index.html',1,'']]],
+  ['releaseemitterpins_7',['releaseEmitterPins',['../classQTRSensors.html#af4fea64349f7d85830b41ded4aa63ce8',1,'QTRSensors']]],
+  ['resetcalibration_8',['resetCalibration',['../classQTRSensors.html#aa840b6ef17562d41edf21ddd08e0672e',1,'QTRSensors']]],
+  ['restypedef_2ecpp_9',['restypedef.cpp',['../restypedef_8cpp.html',1,'']]]
 ];
--- search/functions_11.js
+++ search/functions_11.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['write_0',['write',['../structcmd_8h.html#af2a3ea719b83f672637febdd87c36c36',1,'structcmd.h']]]
+  ['write_0',['write',['../structcmd_8h.html#af2a3ea719b83f672637febdd87c36c36',1,'structcmd.h']]],
+  ['writecalibration_1',['writeCalibration',['../classQTRSensors.html#ad1faa87c08dc2530fc15affd6a59875d',1,'QTRSensors']]]
 ];
